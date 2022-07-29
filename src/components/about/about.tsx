728d279aba8a1f577f42332552e6cc2da6fac9ca
ashuacharya <ashuacharya622@gmail.com>
2022-07-29 20:38:28 +0545

    added skills section

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import me from "../../assets/mobile/ash.png";
 
 const About: React.FC<{}> = () => {
   return (
-    <div className="container">
-      <div className="content"></div>
+    <div className="container about__container">
+      <div className="about__container__content">
+        <h2 className="u-ml">
+          <span>Ashish Acharya,</span>a computer science student
+        </h2>
+        <h3 className="u-ml">
+          Currently studing Csit in Butwal Multiple Campus in 3rd semister.
+        </h3>
+        <h4 className="u-mr">Code</h4>
+        <div className="about__container__content__me">
+          <img src={me} alt="Me" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,9 +1,69 @@
 import React from "react";
+import ts from "../../assets/skills/typescript.svg";
+import express from "../../assets/skills/express.svg";
+import git from "../../assets/skills/git.svg";
+import node from "../../assets/skills/node.svg";
+import figma from "../../assets/skills/figma.svg";
+import react from "../../assets/skills/react.svg";
+import sass from "../../assets/skills/sass.svg";
+import mongodb from "../../assets/skills/mongodb.svg";
 
 const Skills: React.FC<{}> = () => {
   return (
-    <div className="container">
-      <div className="content"></div>
+    <div className="container skills__container">
+      <div className="skills__container__content">
+        <h2 className="skills__container__content__heading">My skills</h2>
+        <ul>
+          <li>
+            <span>Typescript</span>
+            <span>
+              <img src={ts} alt="" />
+            </span>
+          </li>
+          <li>
+            <span>Express</span>
+            <span>
+              <img src={express} alt="" />
+            </span>
+          </li>
+          <li>
+            <span>Git</span>
+            <span>
+              <img src={git} alt="" />
+            </span>
+          </li>
+          <li>
+            <span>Node</span>
+            <span>
+              <img src={node} alt="" />
+            </span>
+          </li>
+          <li>
+            <span>Figma</span>
+            <span>
+              <img src={figma} alt="" />
+            </span>
+          </li>
+          <li>
+            <span>React</span>
+            <span>
+              <img src={react} alt="" />
+            </span>
+          </li>
+          <li>
+            <span>Sass</span>
+            <span>
+              <img src={sass} alt="" />
+            </span>
+          </li>
+          <li>
+            <span>Mongodb</span>
+            <span>
+              <img src={mongodb} alt="" />
+            </span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
