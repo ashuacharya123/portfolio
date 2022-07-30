0b6df8e02d040a2d925eec7823af5405bfcff6a9
ashuacharya <ashuacharya622@gmail.com>
2022-07-30 18:05:09 +0545

    updated logo

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Contact: React.FC<{}> = () => {
   return (
-    <div className="container" id="contact">
-      <div className="content"></div>
+    <div className="container contact__container" id="contact">
+      <div className="contact__container__content"></div>
     </div>
   );
 };
