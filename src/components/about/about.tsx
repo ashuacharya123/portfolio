75049ce8e1333ce499c2fc934716e547b477f273
ashuacharya <ashuacharya622@gmail.com>
2022-08-01 15:06:56 +0545

    done with about section for my pc

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import me from "../../assets/mobile/ash.png";
+import me2 from "../../assets/pc/ash.png";
 
 const About: React.FC<{}> = () => {
+  const [width, setWidth] = React.useState<number>(window.visualViewport.width);
+
+  const resizeHandler = () => {
+    setWidth(window.visualViewport.width);
+  };
+  window.visualViewport.addEventListener("resize", resizeHandler);
   return (
     <div className="container about__container" id="about">
       <div className="about__container__content">
@@ -9,11 +16,12 @@ const About: React.FC<{}> = () => {
           <span>Ashish Acharya,</span>a computer science student
         </h2>
         <h3 className="u-ml u-mr">
-          Currently studing Csit in Butwal Multiple Campus in 3rd semister.
+          Currently studing Csit in Butwal Multiple{" "}
+          <span>Campus in 3rd semister.</span>
         </h3>
         <h4 className="u-mr">Code</h4>
         <div className="about__container__content__me">
-          <img src={me} alt="Me" />
+          <img src={width < 768 ? me : me2} alt="Me" />
         </div>
       </div>
     </div>
