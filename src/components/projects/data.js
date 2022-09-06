768f64f48efb58c2ec23b5b75de9cb31c07f2547
ashuacharya <ashuacharya622@gmail.com>
2022-09-06 22:47:05 +0545

    added paintings project

--- src/components/projects/data.js
+++ src/components/projects/data.js
@@ -6,6 +6,12 @@ export const data = {
     link: "https://tea-ashuacharya123.vercel.app/",
   },
   1: {
+    name: "Paintings",
+    description: " Jam your walls with some creative paintings",
+    image: require("../../assets/projects/paintings.png"),
+    link: "https://paintings-ashuacharya123.vercel.app/",
+  },
+  2: {
     name: "Planets",
     description: "Explore every planet of our solar system",
     image: require("../../assets/projects/planets.png"),
